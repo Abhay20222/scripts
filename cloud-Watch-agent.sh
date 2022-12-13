81e28ddce1b1d66c44e52bb199ed3d3ed6a4428c
Abhay20222 <112698889+Abhay20222@users.noreply.github.com>
2022-12-14 00:40:35 +0530

    Update cloud-Watch-agent.sh

--- cloud-Watch-agent.sh
+++ cloud-Watch-agent.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 wget https://s3.us-east-2.amazonaws.com/amazoncloudwatch-agent-us-east-2/ubuntu/amd64/latest/amazon-cloudwatch-agent.deb
 sudo dpkg -i -E ./amazon-cloudwatch-agent.deb
-sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-config-wizard
+#sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-config-wizard
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 #To boot SSM configuration follow below command
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c ssm:AmazonCloudWatch-linux -s
